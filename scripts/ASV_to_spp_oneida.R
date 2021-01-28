33e1df0371fdc7055366a6824a262d1bf6d1e786
karaandres <kja68@cornell.edu>
2021-01-28 14:40:35 -0500

    Commit from RStudio

--- scripts/ASV_to_spp_oneida.R
+++ scripts/ASV_to_spp_oneida.R
@@ -21,36 +21,37 @@ head(ASV_count_by_sites)
 
 pident_threshold <- 98 # threshold of % sequence identical above which a match is considered valid
 ASV_count_by_sites <- data.frame(ASV_count_by_sites, seqid=unique(blast_dat$seqid),
-                                 family=NA, genus=NA, sscinames=NA, scomnames=NA)
+                                 family=NA, genus=NA, species=NA, scomnames=NA)
 for(seqid in ASV_count_by_sites$seqid) { # for each ASV, subset to the associated blast hits
-    subdat <- blast_dat[blast_dat$seqid == seqid & blast_dat$pident > pident_threshold, ] # subset to hits > pident_threshold
-    if(nrow(subdat)>0) {subdat <- subdat[subdat$pident==max(subdat$pident),]} # subset to top hit(s)
-    subdat2 <- blast_dat[blast_dat$seqid == seqid, ] # subset to hits > pident_threshold
-    subdat2 <- subdat2[subdat2$pident==max(subdat2$pident),]
-    if (nrow(subdat)==0) { # if no hit exceeds pident_threshold
-      ASV_count_by_sites[ASV_count_by_sites$seqid == seqid, c("sscinames", "scomnames")] <- c("NO MATCH", "NO MATCH")
-      if (length(unique(subdat2$genus))==1){ASV_count_by_sites[ASV_count_by_sites$seqid == seqid, "genus"] <- subdat2[1, "genus"]
-      } else {ASV_count_by_sites[ASV_count_by_sites$seqid == seqid, "genus"] <- "NO MATCH"}
-      if (length(unique(subdat2$family))==1){ASV_count_by_sites[ASV_count_by_sites$seqid == seqid, "family"] <- subdat2[1, "family"]
-      } else {ASV_count_by_sites[ASV_count_by_sites$seqid == seqid, "family"] <- "NO MATCH"}
-    } else if (length(unique(subdat$species))==1) { # if single hit or multiple hits > pident_threshold but are the same species
-      ASV_count_by_sites[ASV_count_by_sites$seqid == seqid, c("sscinames", "scomnames","genus","family")] <- subdat[1, c("sscinames", "scomnames","genus","family")]
-    } else {
-      ASV_count_by_sites[ASV_count_by_sites$seqid == seqid, c("sscinames", "scomnames")] <- c("AMBIGUOUS","AMBIGUOUS")
-      if (length(unique(subdat2$genus))==1){ASV_count_by_sites[ASV_count_by_sites$seqid == seqid, "genus"] <- subdat2[1, "genus"]
-      } else {ASV_count_by_sites[ASV_count_by_sites$seqid == seqid, "genus"] <- "AMBIGUOUS"}
-      if (length(unique(subdat2$family))==1){ASV_count_by_sites[ASV_count_by_sites$seqid == seqid, "family"] <- subdat2[1, "family"]
-      } else {ASV_count_by_sites[ASV_count_by_sites$seqid == seqid, "family"] <- "AMBIGUOUS"}
-    }
+  subdat <- blast_dat[blast_dat$seqid == seqid & blast_dat$pident >= pident_threshold, ] # subset to hits > pident_threshold
+  if(nrow(subdat)>0) {subdat <- subdat[subdat$pident==max(subdat$pident),]} # subset to top hit(s)
+  subdat2 <- blast_dat[blast_dat$seqid == seqid, ] # create second subset for higher taxonomic matches
+  subdat2 <- subdat2[subdat2$pident==max(subdat2$pident),] # subset to top hit(s)
+  if (nrow(subdat)==0) { # if no hit exceeds pident_threshold
+    ASV_count_by_sites[ASV_count_by_sites$seqid == seqid, c("species", "scomnames")] <- c("NO MATCH", "NO MATCH")
+    if (length(unique(subdat2$genus))==1){ASV_count_by_sites[ASV_count_by_sites$seqid == seqid, "genus"] <- subdat2[1, "genus"]
+    } else {ASV_count_by_sites[ASV_count_by_sites$seqid == seqid, "genus"] <- "NO MATCH"}
+    if (length(unique(subdat2$family))==1){ASV_count_by_sites[ASV_count_by_sites$seqid == seqid, "family"] <- subdat2[1, "family"]
+    } else {ASV_count_by_sites[ASV_count_by_sites$seqid == seqid, "family"] <- "NO MATCH"}
+  } else if (length(unique(subdat$species))==1) { # if single hit or multiple hits > pident_threshold but are the same species
+    ASV_count_by_sites[ASV_count_by_sites$seqid == seqid, c("species", "scomnames","genus","family")] <- subdat[1, c("species", "scomnames","genus","family")]
+  } else {
+    ASV_count_by_sites[ASV_count_by_sites$seqid == seqid, c("species", "scomnames")] <- c("AMBIGUOUS","AMBIGUOUS")
+    if (length(unique(subdat$genus))==1){ASV_count_by_sites[ASV_count_by_sites$seqid == seqid, "genus"] <- subdat[1, "genus"]
+    } else {ASV_count_by_sites[ASV_count_by_sites$seqid == seqid, "genus"] <- "AMBIGUOUS"}
+    if (length(unique(subdat$family))==1){ASV_count_by_sites[ASV_count_by_sites$seqid == seqid, "family"] <- subdat[1, "family"]
+    } else {ASV_count_by_sites[ASV_count_by_sites$seqid == seqid, "family"] <- "AMBIGUOUS"}
   }
-  
+}
+
 # Restrict output ASVs to those with lengths in ASV_length_range
 lengths <- sapply(1:nrow(ASV_count_by_sites), FUN = function(x) nchar(as.character(ASV_count_by_sites[x,"ASV"])))
 ASV_length_range <- c(160, 192) # lengths of sequences to look at (to eliminate bacteria, etc.)
 ASV_count_by_sites <- ASV_count_by_sites[lengths > ASV_length_range[1]-1 & lengths < ASV_length_range[2]+1, ]
 
-nrow(ASV_count_by_sites[ASV_count_by_sites$scomnames=="AMBIGUOUS",]) # 73
-nrow(ASV_count_by_sites[ASV_count_by_sites$scomnames=="NO MATCH",]) # 725
+nrow(ASV_count_by_sites) # 2982 ASVs
+nrow(ASV_count_by_sites[ASV_count_by_sites$scomnames=="AMBIGUOUS",]) # 73 abiguous matches
+nrow(ASV_count_by_sites[ASV_count_by_sites$scomnames=="NO MATCH",]) # 725 no match exceeding 98% identity
 
 # Write data files
-# write.csv(ASV_count_by_sites, "/Users/kbja10/Documents/Cornell/Research/Oneida/Data_analysis/Datasets/ASV_count_by_sites.csv")
+# write.csv(ASV_count_by_sites, "/Users/kbja10/Documents/Cornell/Research/Oneida/Data_analysis/Datasets/ASV_count_by_sites.csv", row.names=FALSE)
--- scripts/data_filtering_oneida.R
+++ scripts/data_filtering_oneida.R
@@ -7,9 +7,9 @@
 
 # Read in ASV counts by sites: columns are samples, rows are ASVs, taxonomic information in last columns
 ASV_count_by_sites <- read.csv("/Users/kbja10/Documents/Cornell/Research/Oneida/Data_analysis/Datasets/ASV_count_by_sites.csv")
-ASV_count_by_sites <- subset(ASV_count_by_sites, select=-c(X,ASV)) # remove unneeded columns
+ASV_count_by_sites <- subset(ASV_count_by_sites, select=-ASV) # remove unneeded columns
 
-### Remove ASVs with reads below threshold of 0.1% of all reads ###
+### Remove ASVs with reads below threshold of 0.1% of all reads in a sample ###
 ASV_count_by_sites_0.001 <- matrix(NA, nrow = nrow(ASV_count_by_sites), ncol = ncol(ASV_count_by_sites)-5) # create empty matrix to fill
 for (i in 1:(ncol(ASV_count_by_sites)-5)){ # for each column (sample)
   for (j in 1:nrow(ASV_count_by_sites)){ # for each row (ASV)
@@ -50,7 +50,7 @@ for (i in 1:(ncol(ASV_count_by_sites_0.001)-5)){ # for each column (sample)
   for (j in 1:nrow(ASV_count_by_sites_0.001)){ # for each row (ASV)
     if (ASV_count_by_sites_0.001[j,i]<23) { # if number of reads in sample is <23
       ASV_count_by_sites_0.001_adjusted[j,i] <- 0 # replace reads <23 with a 0
-      } else ASV_count_by_sites_0.001_adjusted[j,i] <- ASV_count_by_sites_0.001[j,i] # otherwise they stay the same
+    } else ASV_count_by_sites_0.001_adjusted[j,i] <- ASV_count_by_sites_0.001[j,i] # otherwise they stay the same
   }
 }
 ASV_count_by_sites_0.001_adjusted <- cbind(ASV_count_by_sites_0.001_adjusted, ASV_count_by_sites_0.001[,36:40]) # add taxonomic info back in
@@ -65,17 +65,23 @@ ASV_count_by_sites_cleaned <- ASV_count_by_sites_cleaned[rowSums(ASV_count_by_si
 ASV_count_by_sites_lowest_tax <- ASV_count_by_sites_cleaned
 for (i in 1:nrow(ASV_count_by_sites_cleaned)){ # for each ASV
   # if no species match or ambiguous assignment, assign genus level taxonomic info
-  if (ASV_count_by_sites_cleaned$sscinames[i]=="NO MATCH"|ASV_count_by_sites_cleaned$sscinames[i]=="AMBIGUOUS"){
-    ASV_count_by_sites_lowest_tax$sscinames[i] <- ASV_count_by_sites_cleaned$genus[i]
+  if (ASV_count_by_sites_cleaned$species[i]=="NO MATCH"|ASV_count_by_sites_cleaned$species[i]=="AMBIGUOUS"){
+    ASV_count_by_sites_lowest_tax$species[i] <- ASV_count_by_sites_cleaned$genus[i]
     ASV_count_by_sites_lowest_tax$scomnames[i] <- ASV_count_by_sites_cleaned$genus[i]
   }
   # if no genus level taxonomic assignment, assign family level taxonomic info
   if (ASV_count_by_sites_cleaned$genus[i]=="AMBIGUOUS"){
-    ASV_count_by_sites_lowest_tax$sscinames[i] <- ASV_count_by_sites_cleaned$family[i]
+    ASV_count_by_sites_lowest_tax$species[i] <- ASV_count_by_sites_cleaned$family[i]
     ASV_count_by_sites_lowest_tax$scomnames[i] <- ASV_count_by_sites_cleaned$family[i]
   }
 }
 
-# Sum counts for each species (combine multiple hits)
-sp_count_by_site <- rowsum(ASV_count_by_sites_lowest_tax[,1:29], group = ASV_count_by_sites_lowest_tax$scomnames, na.rm = TRUE)
+# Sum counts for each species (combine ASVs wiht same taxonomic assigment)
 # write.csv(sp_count_by_site, "/Users/kbja10/Documents/Cornell/Research/Oneida/Data_analysis/Datasets/sp_read_count_by_site.csv") # read counts per site by species excluding blanks
+library(dplyr)
+sp_count_by_site <- ASV_count_by_sites_lowest_tax %>% 
+  group_by(scomnames) %>% 
+  summarise(across(where(is.numeric), sum)) %>%
+  left_join(distinct(ASV_count_by_sites_lowest_tax[, c("family", "genus", "species", "scomnames")], scomnames,.keep_all = TRUE)) %>%
+  select(c(family, genus, species), everything())
+# write.csv(sp_count_by_site, "/Users/kbja10/Documents/Cornell/Research/Oneida/Data_analysis/Datasets/sp_read_count_by_site.csv", row.names=FALSE) # read counts per site by species excluding blanks
